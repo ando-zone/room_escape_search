6b34672d3fd0f057cfc8434fab85255ffb9924bb
ando-zone <dev.dohyeonan@gmail.com>
2023-03-16 21:49:20 +0900

    feat: user model 구축하는 과정

--- users/models.py
+++ users/models.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150, default="")
+    is_admin = models.BooleanField(default=False) #non-nullable field (True of False)
