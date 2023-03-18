5482eacb8125cfd436381d9d53b4a396be44c7e4
ando-zone <dev.dohyeonan@gmail.com>
2023-03-18 14:49:41 +0900

    feat: 모든 rooms의 가격을 초기화 하는 admin action 추가

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,8 +3,20 @@ from .models import Room
 # Register your models here.
 
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    # print(model_admin)
+    # print(dir(request))
+    # print(rooms)
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
+
     list_display = [
         "name",
         "price",
