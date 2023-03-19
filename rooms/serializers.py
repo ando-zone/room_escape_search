5e1c7a465af9ffa2b1127f90ee53ae25317aa028
ando-zone <dev.dohyeonan@gmail.com>
2023-03-19 16:54:11 +0900

    feat: SerializerMethodField 사용

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,19 +1,25 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Room
 from brands.serializers import BrandSerializer
 
-class RoomDetailSerializer(ModelSerializer):
+
+class RoomDetailSerializer(serializers.ModelSerializer):
     brand = BrandSerializer(read_only=True)
+    rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
         fields = "__all__"
 
+    def get_rating(self, room):
+        return room.rating()
 
-class RoomListSerializer(ModelSerializer):
+
+class RoomListSerializer(serializers.ModelSerializer):
     # TODO@Ando: brand를 여기서 보여주어야 하나 의문... (location이랑 겹침)
     # 애초에 brand 자체가 필요한지도 더 고민해보기. brand 별 review를 달 수는 있음.
     brand = BrandSerializer()
+    rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -27,5 +33,9 @@ class RoomListSerializer(ModelSerializer):
             "duration_of_time",
             "location",
             "brand",
+            "rating"
         )
         # depth = 1
+
+    def get_rating(self, room):
+        return room.rating()
