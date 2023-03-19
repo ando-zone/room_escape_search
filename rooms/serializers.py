728d95070f67321352f24ebbf8921aa64db5ec9c
ando-zone <dev.dohyeonan@gmail.com>
2023-03-19 12:48:17 +0900

    feat: api/v1/rooms/ 구현

---
+++ rooms/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
+        depth = 1
