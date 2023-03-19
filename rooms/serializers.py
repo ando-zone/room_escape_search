f57280596f63cc5a579caecc5be4934aab1d06d0
ando-zone <dev.dohyeonan@gmail.com>
2023-03-19 13:40:23 +0900

    feat: /api/v1/rooms/ POST request 구현

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -3,14 +3,14 @@ from .models import Room
 from brands.serializers import BrandSerializer
 
 class RoomDetailSerializer(ModelSerializer):
-    brand = BrandSerializer()
+    brand = BrandSerializer(read_only=True)
 
     class Meta:
         model = Room
         fields = "__all__"
 
 
-class RoomSerializer(ModelSerializer):
+class RoomListSerializer(ModelSerializer):
     # TODO@Ando: brand를 여기서 보여주어야 하나 의문... (location이랑 겹침)
     # 애초에 brand 자체가 필요한지도 더 고민해보기. brand 별 review를 달 수는 있음.
     brand = BrandSerializer()
--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,40 @@
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ParseError
 from .models import Room
-from .serializers import RoomSerializer, RoomDetailSerializer
+from brands.models import Brand
+from .serializers import RoomListSerializer, RoomDetailSerializer
+
 
 # Create your views here.
 class Rooms(APIView):
     def get(self, request):
         all_rooms = Room.objects.all()
-        serializer = RoomSerializer(all_rooms, many=True)
+        serializer = RoomListSerializer(all_rooms, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RoomDetailSerializer(data=request.data)
+        if serializer.is_valid():
+            brand_pk = request.data.get("brand")
+
+            if not brand_pk:
+                raise ParseError("Brand is required.")
+
+            try:
+                brand = Brand.objects.get(pk=brand_pk)
+            except Brand.DoesNotExist:
+                raise ParseError("Brand not found.")
+
+            room = serializer.save(
+                brand = brand
+            )
+            serializer = RoomDetailSerializer(room)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
 
 class RoomDetail(APIView):
     def get_object(self, pk):
