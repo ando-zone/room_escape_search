63a424d786a9dbb415a93c9e035f2dd9e72ebf5f
ando-zone <dev.dohyeonan@gmail.com>
2023-03-25 08:27:54 +0900

    feat: secret_key 숨김을 위한 environment files 구현

---
+++ config_django/authentication.py
@@ -0,0 +1,39 @@
+import jwt
+
+from django.conf import settings
+
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from users.models import User
+
+
+class TrustMeBroAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        username = request.headers.get("Trust-Me")
+        if not username:
+            return None
+        try:
+            user = User.objects.get(username=username)
+            return (user, None)
+        except User.DoesNotExist:
+            raise AuthenticationFailed(f"No user {username}")
+
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.headers.get("Jwt")
+        if not token:
+            return None
+        decoded = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=["HS256"],
+        )
+        pk = decoded.get("pk")
+        if not pk:
+            raise AuthenticationFailed("Invalid Token")
+        try:
+            user = User.objects.get(pk=pk)
+            return (user, None)
+        except User.DoesNotExist:
+            raise AuthenticationFailed("User Not Found")
--- config_django/settings.py
+++ config_django/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m+&c9^!e1utj9w+(!m=k&ohj_v2pa++6gj9&255zoqzzg12$b*'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
