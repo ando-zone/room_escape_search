6084eec63f890bb557e2dfcaa2749cd900ae2766
ando-zone <dev.dohyeonan@gmail.com>
2023-03-25 00:20:09 +0900

    feat: token authentication 구축

--- config_django/settings.py
+++ config_django/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 CUSTOM_APPS = [
@@ -144,5 +145,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "config_django.authentications.TrustMeBroAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ]
 }
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
     path("change-password", views.ChangePassword.as_view()),
-    # path("log-in", views.LogIn.as_view()),
-    # path("log-out", views.LogOut.as_view()),
+    path("log-in", views.LogIn.as_view()),
+    path("log-out", views.LogOut.as_view()),
+    path("token-login", obtain_auth_token),
     path("@<str:username>", views.PublicUser.as_view()),
 ]
