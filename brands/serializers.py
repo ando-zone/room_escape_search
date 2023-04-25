e53d158d9205a9c5a80b5e19b18f5d6329997218
ando-zone <dev.dohyeonan@gmail.com>
2023-04-25 23:16:49 +0900

    refactor: brands 앱 수정

--- brands/serializers.py
+++ brands/serializers.py
@@ -1,11 +1,31 @@
 from rest_framework import serializers
 from .models import Brand
+from branches.models import Branch
+from branches import serializers as branch_serializers
+
 
 class BrandSerializer(serializers.ModelSerializer):
+    average_rating = serializers.SerializerMethodField()
+
     class Meta:
         model = Brand
         fields = (
             "pk",
             "name",
-            "description"
+            "description",
+            "average_rating"
         )
+
+    def get_average_rating(self, brand):
+        branches = Branch.objects.filter(brand=brand)
+        total_rating = 0
+        total_branches = 0
+
+        branch_serializer_obj = branch_serializers.BranchSerializer()
+
+        for branch in branches:
+            branch_rating = branch_serializer_obj.get_average_rating(branch)
+            total_rating += branch_rating
+            total_branches += 1
+
+        return total_rating / total_branches if total_branches else 0
--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,4 @@
+from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Brand
@@ -10,4 +11,4 @@ class Brands(APIView):
     def get(self, request):
         all_brands = Brand.objects.all()
         serializer = BrandSerializer(all_brands, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=HTTP_200_OK)
